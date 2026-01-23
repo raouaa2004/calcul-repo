5ce17f3a94e3a38e3c98ba98e95bc71634bf2163
Maha Abdelmalek <mahaabdelmalek2004@gmail.com>
2026-01-23 17:15:29 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -142,9 +142,16 @@ const GradeCalculator = () => {
               <img src="image.png" alt="" style={{width: '100%', height: '100%', objectFit: 'cover'}} />
             </div>
             <div>
-              <h1 style={{ fontSize: '18px', fontWeight: '700', margin: 0, fontFamily: "'Abril Fatface', lineHeight: '1.1' }}>
-                Calculateur de moyenne Master
-              </h1>
+            <h1 style={{ 
+  fontSize: '18px', 
+  fontWeight: '700', 
+  margin: 0, 
+  fontFamily: 'Abril Fatface',  // uniquement la police
+  lineHeight: 1.1                // séparé de fontFamily
+}}>
+  Calculateur de moyenne Master
+</h1>
+
               <p style={{fontSize: '10px', opacity: 0.9}}>Département d'Informatique - Tébessa</p>
             </div>
           </div>
